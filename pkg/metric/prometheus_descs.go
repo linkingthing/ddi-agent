87534dfcff16281b34b885bd9a00e6160381abf3
yuhaijiao <yuhj@linkingthing.com>
2020-05-28 11:41:19 +0000

    update prometheus desc name

--- pkg/metric/prometheus_descs.go
+++ pkg/metric/prometheus_descs.go
@@ -5,6 +5,12 @@ import (
 )
 
 const (
+	MetricLabelNode     = "node"
+	MetricLabelType     = "type"
+	MetricLabelView     = "view"
+	MetricLabelRcode    = "rcode"
+	MetricLabelSubnetId = "subnet_id"
+
 	MetricNameDNSQPS             = "lx_dns_qps"
 	MetricNameDNSQueriesTotal    = "lx_dns_queries_total"
 	MetricNameDNSQueryTypeRatios = "lx_dns_query_type_ratios"
@@ -14,20 +20,20 @@ const (
 	MetricNameDHCPLPS          = "lx_dhcp_lps"
 	MetricNameDHCPPacketsStats = "lx_dhcp_packets_stats"
 	MetricNameDHCPLeasesTotal  = "lx_dhcp_leases_total"
-	MetricNameDUsages          = "lx_dhcp_usages"
+	MetricNameDHCPUsages       = "lx_dhcp_usages"
 )
 
 var (
-	DNSQPS             = prometheus.NewDesc("lx_dns_qps", "dns qps per node", []string{"node"}, nil)
-	DNSQueriesTotal    = prometheus.NewDesc("lx_dns_queries_total", "dns queries per node", []string{"node"}, nil)
-	DNSQueryTypeRatios = prometheus.NewDesc("lx_dns_qtype_ratios", "dns qtypes ratio per node,type", []string{"node", "type"}, nil)
-	DNSCacheHits       = prometheus.NewDesc("lx_dns_cache_hits", "dns cache hits per node,view", []string{"node", "view"}, nil)
-	DNSResolvedRatios  = prometheus.NewDesc("lx_dns_resolved_ratios", "dns resolve ratio per node,rcode", []string{"node", "rcode"}, nil)
+	DNSQPS             = prometheus.NewDesc(MetricNameDNSQPS, "dns qps per node", []string{MetricLabelNode}, nil)
+	DNSQueriesTotal    = prometheus.NewDesc(MetricNameDNSQueriesTotal, "dns queries per node", []string{MetricLabelNode}, nil)
+	DNSQueryTypeRatios = prometheus.NewDesc(MetricNameDNSQueryTypeRatios, "dns qtypes ratio per node,type", []string{MetricLabelNode, MetricLabelType}, nil)
+	DNSCacheHits       = prometheus.NewDesc(MetricNameDNSCacheHits, "dns cache hits per node,view", []string{MetricLabelNode, MetricLabelView}, nil)
+	DNSResolvedRatios  = prometheus.NewDesc(MetricNameDNSResolvedRatios, "dns resolve ratio per node,rcode", []string{MetricLabelNode, MetricLabelRcode}, nil)
 
-	DHCPLPS          = prometheus.NewDesc("lx_dhcp_lps", "dhcp lps per node", []string{"node"}, nil)
-	DHCPPacketsStats = prometheus.NewDesc("lx_dhcp_packets_stats", "dhcp packets stats per node,type", []string{"node", "type"}, nil)
-	DHCPLeasesTotal  = prometheus.NewDesc("lx_dhcp_leases_total", "dhcp leases statistic per node", []string{"node"}, nil)
-	DHCPUsages       = prometheus.NewDesc("lx_dhcp_usages", "dhcp usages statistic per node,subnet", []string{"node", "subnet_id"}, nil)
+	DHCPLPS          = prometheus.NewDesc(MetricNameDHCPLPS, "dhcp lps per node", []string{MetricLabelNode}, nil)
+	DHCPPacketsStats = prometheus.NewDesc(MetricNameDHCPPacketsStats, "dhcp packets stats per node,type", []string{MetricLabelNode, MetricLabelType}, nil)
+	DHCPLeasesTotal  = prometheus.NewDesc(MetricNameDHCPLeasesTotal, "dhcp leases statistic per node", []string{MetricLabelNode}, nil)
+	DHCPUsages       = prometheus.NewDesc(MetricNameDHCPUsages, "dhcp usages statistic per node,subnet", []string{MetricLabelNode, MetricLabelSubnetId}, nil)
 )
 
 var DNSPrometheusDescs = []*prometheus.Desc{DNSQPS, DNSQueriesTotal, DNSQueryTypeRatios, DNSCacheHits, DNSResolvedRatios}
