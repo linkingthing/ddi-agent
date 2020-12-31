314bbee6eaee1f447d6dec149ff28b159364bd07
Kseleven <panjunke2009@sina.com>
2020-12-31 09:58:48 +0800

    kafka cmd

---
+++ pkg/dns/kafkaconsumer/kafkacmd.go
@@ -0,0 +1,41 @@
+package kafkaconsumer
+
+const (
+	DNSTopic = "dns"
+
+	StartDNS = "start_dns"
+	StopDNS  = "stop_dns"
+
+	CreateACL = "create_acl"
+	UpdateACL = "update_acl"
+	DeleteACL = "delete_acl"
+
+	CreateView = "create_view"
+	UpdateView = "update_view"
+	DeleteView = "delete_view"
+
+	CreateZone = "create_zone"
+	UpdateZone = "update_zone"
+	DeleteZone = "delete_zone"
+
+	CreateForwardZone = "create_forwardzone"
+	UpdateForwardZone = "update_forwardzone"
+	DeleteForwardZone = "delete_forwardzone"
+	FlushForwardZone  = "flush_forwardzone"
+
+	CreateRR = "create_rr"
+	UpdateRR = "update_rr"
+	DeleteRR = "delete_rr"
+
+	CreateRedirection = "create_redirection"
+	UpdateRedirection = "update_redirection"
+	DeleteRedirection = "delete_redirection"
+
+	CreateUrlRedirect = "create_urlredirect"
+	UpdateUrlRedirect = "update_urlredirect"
+	DeleteUrlRedirect = "delete_urlredirect"
+
+	UpdateGlobalConfig = "update_globalconfig"
+
+	UploadLog = "upload_dnslog"
+)
